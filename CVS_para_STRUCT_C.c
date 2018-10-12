22740c4849b3ca5f99fa7969121e8e253836fdb6
rodalvis <31355659+rodalvis@users.noreply.github.com>
2018-10-12 16:46:04 -0300

    Update CVS_para_STRUCT_C.c

--- CVS_para_STRUCT_C.c
+++ CVS_para_STRUCT_C.c
@@ -1,35 +1,73 @@
 #include <stdio.h>
+#include <locale.h>
 
 typedef struct{
-	int id;
-	char classe[25];
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	float f;
+	float g;
+	int h;
+	//int i;
+	char classe[17];
 }diabetes;
  
- diabetes teste[5];
+ diabetes teste[769];
 
 int main(){
- 
+ //setlocale(LC_ALL,"");
 	FILE *arquivo;
 	arquivo = fopen("teste.txt","r");
-	
+	//system("pwd");
 	int i =0;
 	int j = 0;
-	//char s[27];
+	int temp=0;
 	
 	if(arquivo == NULL) 
-		printf("erro"); 	
+		printf("erro ao abrir"); 	
 	else {
 	while(!feof(arquivo)){
-		fscanf(arquivo,"%d[^,]",&teste[i].id);
+		fscanf(arquivo,"%d[^,]",&teste[i].a);
+		//fscanf(arquivo,"%d[^,]",&temp);
+		//teste[i].a = temp % 100 ;
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%d[^,]",&teste[i].b);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%d[^,]",&teste[i].c);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%d[^,]",&teste[i].d);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%d[^,]",&teste[i].e);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%f2[^,]",&teste[i].f);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%f2[^,]",&teste[i].g);
+		fseek(arquivo,+1,SEEK_CUR);
+		fscanf(arquivo,"%d[^,]",&teste[i].h);
+		fseek(arquivo,+1,SEEK_CUR);
 		fscanf(arquivo,"%[^\n]%*c",&teste[i].classe);
+
+		printf("%1d ",teste[i].a);
+		printf("%1d ",teste[i].b);
+		printf("%1d ",teste[i].c);
+		printf("%1d ",teste[i].d);
+		printf("%1d ",teste[i].e);
+		printf("%.2f ",teste[i].f);
+		printf("%.3f ",teste[i].g);
+		printf("%1d ",teste[i].h);
+		//printf("%d",teste[i].i);
+	printf("%s\n",teste[i].classe);
+
 
 	i++;
 	}	}
 	i--;
 	
 	for(;j<=i;j++){
-		printf("%d",teste[j].id);
-		printf("%s\n",teste[j].classe);
+
+		//printf("%s\n",teste[j].classe);
 		
 		}
 	return  0;
